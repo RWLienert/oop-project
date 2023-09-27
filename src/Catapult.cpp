bee9aec2df3c654e98c20bd4fd487a7772dd3803
RWLienert <rileylienert@gmail.com>
2023-09-27 15:13:05 +0930

    Wrote the catapult class

---
+++ src/Catapult.cpp
@@ -0,0 +1,23 @@
+#include "Catapult.h"
+
+Catapult::Catapult(int width, int height){
+    // initialise values
+    this->width = width;
+    this->height = height;
+    // create body
+    body = new RectangleShape(Vector2f(width,height));
+    body->setOrigin(width*0.8,height/2);
+    body->setFillColor(Color(57, 58, 59));
+    alive = false;
+}
+
+void Catapult::spawn(int cornerX, int cornerY){
+    //define positioning
+    body->setPosition(cornerX,cornerY);
+    alive = true;
+    pivot = Vector2f(cornerX,cornerY);
+}
+
+Catapult::~Catapult(){
+    delete this->body;
+}
---
+++ src/Catapult.h
@@ -0,0 +1,19 @@
+#ifndef CATAPULT_H
+#define CATAPULT_H
+
+#include <SFML/Graphics.hpp>
+#include "Entity.h"
+#include <iostream>
+using namespace sf;
+using namespace std;
+
+class Catapult : public Entity{
+    protected:
+        Vector2f pivot;
+    public:
+        Catapult(int width, int height);
+        void spawn(int cornerX, int cornerY);
+        ~Catapult();
+};
+
+#endif
