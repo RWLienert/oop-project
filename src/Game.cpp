3274ffa5d91deb07e8a78b9557f11e191ca2078a
RWLienert <rileylienert@gmail.com>
2023-09-29 12:16:23 +0930

    Changed variable names

--- src/Game.cpp
+++ src/Game.cpp
@@ -5,7 +5,7 @@
 Game::Game(int width, int height,string title){
     win = new RenderWindow(VideoMode(width,height),title);
     win->setFramerateLimit(60);
-    castle = new Castle(100,100,100);
+    castle = new Castle(4,140,140,"resources/castle.png");
     arrow = new Arrow("resources/arrow.png");
 }
 
@@ -18,7 +18,7 @@ void Game::load(){
 void Game::moveCatapult(Vector2f translatedPos, int currentCatapult){
     if (clickOn == true) {
         // math to find the required angle to move the catapult
-        Vector2f currentPosition = castle->getCatapults()[currentCatapult]->getBody()->getPosition();
+        Vector2f currentPosition = castle->getCatapults()[currentCatapult]->getSprite()->getPosition();
         const float PI = 3.14159265;
         float difX = currentPosition.x - translatedPos.x;
         float difY = currentPosition.y - translatedPos.y;
@@ -27,7 +27,6 @@ void Game::moveCatapult(Vector2f translatedPos, int currentCatapult){
         float scaleArrow = sqrt(difX*difX+difY*difY);
         if (scaleArrow > 300) {
             scaleArrow = 300;
-            arrow->getSprite()->setColor(Color::Red);
         }
 
         // alter the arrow's length and colour based on position
@@ -37,7 +36,7 @@ void Game::moveCatapult(Vector2f translatedPos, int currentCatapult){
 
         // calculate rotation and apply it to the objects
         float rotation = (atan2(difY,difX))* 180/PI;
-        castle->getCatapults()[currentCatapult]->getBody()->setRotation(rotation);
+        castle->getCatapults()[currentCatapult]->getSprite()->setRotation(rotation);
         arrow->getSprite()->setRotation(rotation);
         arrow->getBar()->setRotation(rotation);
     }
@@ -61,7 +60,7 @@ void Game::run(){
             if (Mouse::isButtonPressed(Mouse::Left)){
                 for (int i = 0; i < 4; i++){
                     // check whether mouse cursor overlaps a catapult's outline
-                    if (castle->getCatapults()[i]->getBody()->getGlobalBounds().contains(translatedPos)){
+                    if (castle->getCatapults()[i]->getSprite()->getGlobalBounds().contains(translatedPos)){
                         arrow->spawn(castle->getCatapults()[i]->getPivot().x,castle->getCatapults()[i]->getPivot().y);
                         currentCatapult = i;
                         clickOn = true;
