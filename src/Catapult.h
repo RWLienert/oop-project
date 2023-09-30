f78bfca7f8789d1a59933194e95c80f060cc5a37
RWLienert <rileylienert@gmail.com>
2023-09-30 15:08:57 +0930

    Updated variables

--- src/Catapult.h
+++ src/Catapult.h
@@ -2,7 +2,9 @@
 #define CATAPULT_H
 
 #include <SFML/Graphics.hpp>
+#include <SFML/System/Vector2.hpp>
 #include "Entity.h"
+#include "Fireball.h"
 #include <iostream>
 using namespace sf;
 using namespace std;
@@ -10,10 +12,22 @@ using namespace std;
 class Catapult : public Entity{
     protected:
         Vector2f pivot;
+        Vector2f end;
+        Vector2f startPos;
+        Vector2f direction;
+        Fireball** fireballs;
+        int numBalls;
+        int remainingBalls;
+        float power;
     public:
-        Catapult(int width, int height, string imgDirectory);
+        Catapult(int width, int height, string imgDirectory, int numBalls);
         void spawn(int cornerX, int cornerY);
         void draw(RenderWindow* win);
+        void fire(float endX, float endY);
+        void reload();
+        void updateFire(float power, Vector2f startPos, Vector2f direction);
+        int remaining();
+        void setRemaining();
         Sprite* getSprite();
         Vector2f getPivot();
         ~Catapult();
