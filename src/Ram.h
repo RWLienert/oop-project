f4705e2526b2f1ba1083feea99afd1d7d9bdd726
RWLienert <rileylienert@gmail.com>
2023-10-08 19:19:58 +1030

    Created Ram class

---
+++ src/Ram.h
@@ -0,0 +1,14 @@
+#ifndef RAM_H
+#define RAM_H
+
+#include <SFML/Graphics.hpp>
+#include "Enemy.h"
+#include <iostream>
+using namespace sf;
+using namespace std;
+
+class Ram : public Enemy{
+
+};
+
+#endif
