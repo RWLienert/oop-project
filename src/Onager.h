cc2b9b25a3f1b9d2c903a9ca7c5b420df6dc52e7
RWLienert <rileylienert@gmail.com>
2023-10-08 19:19:28 +1030

    Created Onager class

---
+++ src/Onager.h
@@ -0,0 +1,14 @@
+#ifndef ONAGER_H
+#define ONAGER_H
+
+#include <SFML/Graphics.hpp>
+#include "Enemy.h"
+#include <iostream>
+using namespace sf;
+using namespace std;
+
+class Onager : public Enemy{
+    
+};
+
+#endif
