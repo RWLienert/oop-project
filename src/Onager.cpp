92c264f70a564ef986fae73cd030b3d59b940177
RWLienert <rileylienert@gmail.com>
2023-10-09 14:15:30 +1030

    Adjusted spawning and creation

--- src/Onager.cpp
+++ src/Onager.cpp
@@ -1,1 +1,29 @@
 #include "Onager.h"
+
+Onager::Onager(int width, int height){
+    this->width = width;
+    this->height = height;
+    body = new RectangleShape(Vector2f(30.f,30.f));
+    body->setOrigin(15.f,15.f);
+    body->setFillColor(Color(132, 138, 133));
+    body->setPosition(-100,-100);
+    alive = false;
+    srand(time(NULL));
+}
+
+void Onager::draw(RenderWindow* win){
+    if (alive == true){
+        win->draw(*body);
+    }
+}
+
+void Onager::spawn(int winX, int winY){
+    int onagerRandX = rand() % (winX - 20);
+    int onagerRandY = rand() % (winY - 20);
+    body->setPosition(static_cast<float>(onagerRandX), static_cast<float>(onagerRandY));
+    alive = true;
+}
+
+Onager::~Onager(){
+    delete body;
+}
--- src/Onager.h
+++ src/Onager.h
@@ -3,12 +3,21 @@
 
 #include <SFML/Graphics.hpp>
 #include "Enemy.h"
+#include <vector>
+#include <ctime>
 #include <iostream>
 using namespace sf;
 using namespace std;
 
 class Onager : public Enemy{
-    
+    protected:
+        Shape* body;
+        int maxOnagers;
+    public:
+        Onager(int width, int height);
+        void draw(RenderWindow* win);
+        void spawn(int winX, int winY);
+        ~Onager();
 };
 
 #endif
