b467912f5e95f88ecf551ca9ef71c8ae6743b592
RWLienert <rileylienert@gmail.com>
2023-10-09 14:17:35 +1030

    Adjusted test

--- tests/castletests.cpp
+++ tests/castletests.cpp
@@ -2,7 +2,7 @@
 #include "../src/Castle.h"
 
 Test(castletests, create) {
-    Castle castle(100,80,80);
+    Castle castle(80,80,"../resources/castle.png");
     cr_expect("Castle class initialisation broke");
     castle.spawn(1000,1000);
     cr_expect("Castle isn't spawning");
