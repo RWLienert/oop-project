7b15ebda62b8b0436fee26875b296954e8381aa7
RWLienert <rileylienert@gmail.com>
2023-10-17 18:45:22 +1030

    Deleted unnecessary testing folder

--- tests/castletests.cpp
+++
@@ -1,9 +0,0 @@
-#include <criterion/criterion.h>
-#include "../src/Castle.h"
-
-Test(castletests, create) {
-    Castle castle(80,80,"../resources/castle.png");
-    cr_expect("Castle class initialisation broke");
-    castle.spawn(1000,1000);
-    cr_expect("Castle isn't spawning");
-}
